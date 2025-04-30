6382241f8da4f255df6d7fb69d97a0cfde156d08
Tuyen Dang <tuyendq@gmail.com>
2025-04-30 23:46:57 +0700

    try extracting videoId

--- src/extract-property.py
+++ src/extract-property.py
@@ -20,14 +20,17 @@ with open("D:\projects\lthwbs\ytInitialData.json", "r", encoding="utf-8") as fil
     # for content in contents:
     #     print(content["richItemRenderer"]["content"]["videoRenderer"]["title"])
   
+    count = 0
     for content in contents:
         if "richItemRenderer" in content:
             rich_item = content["richItemRenderer"]
             if "content" in rich_item and "videoRenderer" in rich_item["content"]:
                 video_renderer = rich_item["content"]["videoRenderer"]
-                if "title" in video_renderer: # and "simpleText" in video_renderer["title"]:
-                    title = video_renderer["title"] #["simpleText"]
-                    print(title)
+                if "title" in video_renderer and "videoId" in video_renderer and "text" in video_renderer["title"]["runs"][0]:
+                    videoId = video_renderer["videoId"]
+                    title = video_renderer["title"]["runs"][0]["text"]
+                    count += 1
+                    print(f"Video {count}: {title} - {videoId}")
                 else:
                     print("Title not found")
             else:
